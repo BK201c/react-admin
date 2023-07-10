6a7bb04f2271c92bbe70c51fff562d0a549f8621
bk201c <bk201c@outlook.com>
2023-07-10 14:30:52 +0800

    删除冗余

---
+++ src/pages/app/MotManagement/BinMonitor/index.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+export default class Detail extends React.Component<RendererProps, any> {
+  render() {
+    return <div></div>;
+  }
+}
---
+++ src/pages/app/MotManagement/MapMonitor/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function EmbeddedPage() {
+  return (
+    <div>
+      <iframe src="https://www.example.com" title="Embedded Page" />
+    </div>
+  );
+}
+
+export default EmbeddedPage;
